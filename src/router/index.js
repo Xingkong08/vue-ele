b9e6017770faa8daf2f069e2826f942cadef2b2a
JustFeel <49368040+Xingkong08@users.noreply.github.com>
2020-05-28 22:37:46 +0800

    初步精简

--- src/router/index.js
+++ src/router/index.js
@@ -5,38 +5,36 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/Video',
-    name: 'Video',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Video.vue')
-  },
-  {
-    path: '/Reader',
-    name: 'Reader',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Reader')
-  },
-  {
-    path: '/Book',
-    name: 'FrameReader',
-    component: () => import(/* webpackChunkName: "about" */ '../views/FrameReader.vue')
-  },
-  {
-    path: '/Notice',
-    name: 'Notice',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Notice.vue')
-  },
-]
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/Video',
+        name: 'Video',
+        component: () => import(/* webpackChunkName: "about" */ '../views/Video.vue')
+    },
+    {
+        path: '/Reader',
+        name: 'Reader',
+        component: () => import(/* webpackChunkName: "about" */ '../views/Reader')
+    },
+    {
+        path: '/Book',
+        name: 'FrameReader',
+        component: () => import(/* webpackChunkName: "about" */ '../views/FrameReader.vue')
+    },
+    {
+        path: '/Notice',
+        name: 'Notice',
+
+        component: () => import(/* webpackChunkName: "about" */ '../views/Notice.vue')
+    },
+];
 
 const router = new VueRouter({
-  routes
-})
+    routes
+});
 
 export default router
